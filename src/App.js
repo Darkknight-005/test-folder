c80a99eb14955fb0319f4c62a560e6ae594cd33a
darkknight-005 <gauravmaurya61@yahoo.in>
2022-07-15 13:11:04 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import Landing_page from './Components/landing_page';
 import { HashRouter, Routes, Route} from "react-router-dom"
 import SignUp from './Components/signUp';
-// import Feed from './Components/feed';
+import Feed from './Components/feed.jsx';
 
 function App() {
   return (
     <HashRouter >
 
       <Routes>
-      <Route exact path="/" element={<Landing_page />} />
-      <Route exact path="/sign-up" element={<SignUp />} />
-
+        <Route exact path="/" element={<Landing_page />} />
+        <Route exact path="/sign-up" element={<SignUp />} />
+        <Route exact path="/feed" element={<Feed />} />
       </Routes>
 
     </HashRouter>
